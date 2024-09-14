d6afba7ef7122828b7ba367cfb6d6e995aa5f500
Diego Larrea <larreadi@personal.com.py>
2024-09-14 13:23:31 -0400

    fix: filtro de busqueda de notas de credito

--- src/services/notaDeCreditoService.js
+++ src/services/notaDeCreditoService.js
@@ -12,24 +12,24 @@ const emitirNotaDeCredito = async (datos, datosUsuario) => {
     const establecimiento = await prisma.establecimiento.findFirst({
       where: {
         codigo: datos.establecimiento,
-        empresa_id: datosUsuario.empresaId
-      }
-    })
+        empresa_id: datosUsuario.empresaId,
+      },
+    });
 
-    if(!establecimiento) {
-      throw new ErrorApp('No se encontró establecimiento', 404)
+    if (!establecimiento) {
+      throw new ErrorApp("No se encontró establecimiento", 404);
     }
-    
+
     // Buscar caja para establecimiento
     const caja = await prisma.caja.findFirst({
       where: {
         codigo: datos.caja,
-        establecimiento_id: establecimiento.id
-      }
-    })
+        establecimiento_id: establecimiento.id,
+      },
+    });
 
-    if(!caja) {
-      throw new ErrorApp('No se encontró caja', 404)
+    if (!caja) {
+      throw new ErrorApp("No se encontró caja", 404);
     }
 
     //Buscar datos del usuario
@@ -48,20 +48,20 @@ const emitirNotaDeCredito = async (datos, datosUsuario) => {
     const factura = await prisma.factura.findFirst({
       where: {
         cdc: datos.cdc,
-        condicion_venta: 'CONTADO',
-        sifen_estado: {not: 'Cancelado'}
+        condicion_venta: "CONTADO",
+        sifen_estado: { not: "Cancelado" },
       },
       include: {
         cliente_empresa: {
           include: {
-            cliente: true
-          }
-        }
-      }
-    })
+            cliente: true,
+          },
+        },
+      },
+    });
 
-    if(!factura) {
-      throw new ErrorApp('No se encontró cdc', 404)
+    if (!factura) {
+      throw new ErrorApp("No se encontró cdc", 404);
     }
 
     // Verificar cálculos
@@ -100,52 +100,57 @@ const emitirNotaDeCredito = async (datos, datosUsuario) => {
     // Buscar si ya hay nota de crédito para la factura dada
     const notasDeCredito = await prisma.notaCredito.findMany({
       where: {
-        factura_id: factura.id
-      }
-    })
+        factura_id: factura.id,
+      },
+    });
 
     // Verificar que el total de las notas de crédito anteriores más el de ahora no supere el total de la factura
-    if(notasDeCredito && notasDeCredito.length > 0){
-      let totalNotasDeCredito = total
-      notasDeCredito.forEach(e => {
-        totalNotasDeCredito += Number(e.total)
-      })
+    if (notasDeCredito && notasDeCredito.length > 0) {
+      let totalNotasDeCredito = total;
+      notasDeCredito.forEach((e) => {
+        totalNotasDeCredito += Number(e.total);
+      });
 
-      if(totalNotasDeCredito > factura.total){
-        throw new ErrorApp('El total de las notas de crédito supera el valor total de la factura', 400)
+      if (totalNotasDeCredito > factura.total) {
+        throw new ErrorApp(
+          "El total de las notas de crédito supera el valor total de la factura",
+          400
+        );
       }
-
     }
 
     // Datos adicionales
-    const notaDeCreditoUuid = uuidv4()
+    const notaDeCreditoUuid = uuidv4();
 
     // Se usa transacción y FOR UPDATE para bloquear la tabla al crear el número de factura por si hay concurrencia
     const notaDeCredito = await prisma.$transaction(async (tx) => {
-      const secuencia = await tx.$queryRaw`SELECT valor FROM secuencia_nota_credito WHERE caja_id = ${caja.id} FOR UPDATE`
+      const secuencia =
+        await tx.$queryRaw`SELECT valor FROM secuencia_nota_credito WHERE caja_id = ${caja.id} FOR UPDATE`;
 
       if (!secuencia || secuencia.length === 0) {
-        throw new ErrorApp('Secuencia no encontrada', 404)
+        throw new ErrorApp("Secuencia no encontrada", 404);
       }
 
       const numeroNotaDeCredito = Number(secuencia[0].valor) + 1;
-      await tx.$executeRaw`UPDATE secuencia_nota_credito SET valor = ${numeroNotaDeCredito} WHERE caja_id = ${caja.id}`
+      await tx.$executeRaw`UPDATE secuencia_nota_credito SET valor = ${numeroNotaDeCredito} WHERE caja_id = ${caja.id}`;
 
       const codigosSeguridadRaw = await prisma.notaCredito.findMany({
         select: {
           codigo_seguridad: true,
         },
         where: {
-          caja_id: caja.id
-        }
-      })
+          caja_id: caja.id,
+        },
+      });
 
-      const codigosSeguridad = codigosSeguridadRaw.map((e) => e.codigo_seguridad)
+      const codigosSeguridad = codigosSeguridadRaw.map(
+        (e) => e.codigo_seguridad
+      );
 
-      let codigoSeguridadAleatorio = generarCodigoSeguridad()
-  
+      let codigoSeguridadAleatorio = generarCodigoSeguridad();
+
       while (codigosSeguridad.includes(codigoSeguridadAleatorio)) {
-        codigoSeguridadAleatorio = generarCodigoSeguridad()
+        codigoSeguridadAleatorio = generarCodigoSeguridad();
       }
 
       // Llamar a la API de facturación
@@ -156,11 +161,11 @@ const emitirNotaDeCredito = async (datos, datosUsuario) => {
         numeroNotaDeCredito,
         empresaRuc: usuario.empresa.ruc,
         clienteRuc: factura.cliente_empresa.cliente.ruc,
-        clienteNombre: factura.cliente_empresa.cliente.nombres
-      })
+        clienteNombre: factura.cliente_empresa.cliente.nombres,
+      });
 
       if (!resultado || resultado.status != true) {
-        throw new ErrorApp("Error al generar nota de crédito", 500)
+        throw new ErrorApp("Error al generar nota de crédito", 500);
       }
 
       // Crear nota de crédito
@@ -177,12 +182,12 @@ const emitirNotaDeCredito = async (datos, datosUsuario) => {
           codigo_seguridad: codigoSeguridadAleatorio,
           numero_nota_credito: numeroNotaDeCredito,
           usuario_id: usuario.id,
-          caja_id: caja.id
-        }
-      })
+          caja_id: caja.id,
+        },
+      });
 
       // Agregar detalles de nota de crédito
-      const datosNotaDeCreditoDetalle = datos.items.map(e => ({
+      const datosNotaDeCreditoDetalle = datos.items.map((e) => ({
         nota_credito_id: notaDeCredito.id,
         cantidad: Number(e.cantidad),
         precio_unitario: e.precioUnitario,
@@ -190,38 +195,40 @@ const emitirNotaDeCredito = async (datos, datosUsuario) => {
         impuesto: e.impuesto,
         total: e.total,
         descripcion: e.descripcion,
-      }))
+      }));
 
       const notaDeCreditoDetalle = await prisma.notaCreditoDetalle.createMany({
-        data: datosNotaDeCreditoDetalle
-      })
+        data: datosNotaDeCreditoDetalle,
+      });
 
-      return notaDeCredito
-    })
+      return notaDeCredito;
+    });
 
     // Crear PDF
 
-    return notaDeCredito
-
+    return notaDeCredito;
   } catch (error) {
     console.log(error);
-    ErrorApp.handleServiceError(error)
+    ErrorApp.handleServiceError(error);
   }
-}
+};
 
 const apiFacturacionElectronicaNotaDeCredito = async (datos) => {
   // return {status: true, recordID: '123', cdc: 'test', link: 'test', xmlLink: 'test'}
-  
+
   const form = new FormData();
 
-  let pagos = [{
-    name: "cash",
-    tipoPago: "99", // 1 (efectivo), 3 (TC), 4 (TD), 99 (Otros)
-    monto: Number(datos.total),
-  }]
+  let pagos = [
+    {
+      name: "cash",
+      tipoPago: "99", // 1 (efectivo), 3 (TC), 4 (TD), 99 (Otros)
+      monto: Number(datos.total),
+    },
+  ];
 
   const items = datos.items.map((e) => {
-    const baseGravItem = e.tasa == "0%" ? 0 : Number(e.total) - Number(e.impuesto);
+    const baseGravItem =
+      e.tasa == "0%" ? 0 : Number(e.total) - Number(e.impuesto);
     const ivaTasa = e.tasa == "0%" ? 0 : e.tasa == "5%" ? 5 : 10;
     const ivaAfecta = e.tasa == "0%" ? 3 : 1;
 
@@ -246,7 +253,7 @@ const apiFacturacionElectronicaNotaDeCredito = async (datos) => {
     documentoAsociado: {
       remision: false,
       tipoDocumento: 1,
-      cdcAsociado: datos.cdc
+      cdcAsociado: datos.cdc,
     },
     establecimiento: datos.establecimiento,
     punto: datos.caja,
@@ -289,8 +296,7 @@ const apiFacturacionElectronicaNotaDeCredito = async (datos) => {
   console.log(resultado);
 
   return resultado;
-
-}
+};
 
 const generarCodigoSeguridad = (length = 9) => {
   let result = "";
@@ -300,34 +306,39 @@ const generarCodigoSeguridad = (length = 9) => {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
-}
+};
 
-const getNotasDeCredito = async (page = 1, itemsPerPage = 10, filter = null, empresaId) => {
+const getNotasDeCredito = async (
+  page = 1,
+  itemsPerPage = 10,
+  filter = null,
+  empresaId
+) => {
   try {
     const skip = (page - 1) * itemsPerPage;
     const take = itemsPerPage;
 
     const establecimientos = await prisma.establecimiento.findMany({
       where: {
-        empresa_id: empresaId
+        empresa_id: empresaId,
       },
       include: {
-        cajas: true
-      }
-    })
+        cajas: true,
+      },
+    });
 
-    const cajasIds = []
+    const cajasIds = [];
 
-    for(const establecimiento of establecimientos){
-      for(const caja of establecimiento.cajas) {
-        cajasIds.push(caja.id)
+    for (const establecimiento of establecimientos) {
+      for (const caja of establecimiento.cajas) {
+        cajasIds.push(caja.id);
       }
     }
 
-    let notasCredito = []
-    let totalItems = 0
+    let notasCredito = [];
+    let totalItems = 0;
 
-    if(filter === null) {
+    if (filter === null) {
       notasCredito = await prisma.notaCredito.findMany({
         skip,
         take,
@@ -336,22 +347,21 @@ const getNotasDeCredito = async (page = 1, itemsPerPage = 10, filter = null, emp
         },
         where: {
           caja_id: {
-            in: cajasIds
+            in: cajasIds,
           },
         },
         include: {
-          factura: true
-        }
-      })
+          factura: true,
+        },
+      });
 
       totalItems = await prisma.notaCredito.count({
         where: {
           caja_id: {
-            in: cajasIds
+            in: cajasIds,
           },
         },
-      })
-
+      });
     } else {
       const factura = await prisma.factura.findFirst({
         skip,
@@ -360,25 +370,30 @@ const getNotasDeCredito = async (page = 1, itemsPerPage = 10, filter = null, emp
           fecha_creacion: "desc",
         },
         where: {
-          cdc: filter
+          OR: [
+            { cdc: filter },
+            { numero_factura: !isNaN(filter) && String(filter).length <= 7 ? Number(filter) : 0 },
+          ],
         },
-      })
+      });
 
-      notasCredito = await prisma.notaCredito.findMany({
-        where: {
-          factura_id: factura.id
-        },
-        include: {
-          factura: true
-        }
-      })
+      if (factura) {
+        notasCredito = await prisma.notaCredito.findMany({
+          where: {
+            factura_id: factura.id,
+          },
+          include: {
+            factura: true,
+          },
+        });
 
-      totalItems = await prisma.notaCredito.count({
-        where: {
-          factura_id: factura.id
-        }
-      })
-    } 
+        totalItems = await prisma.notaCredito.count({
+          where: {
+            factura_id: factura.id,
+          },
+        });
+      }
+    }
 
     return {
       items: notasCredito,
@@ -387,12 +402,12 @@ const getNotasDeCredito = async (page = 1, itemsPerPage = 10, filter = null, emp
       totalItems,
     };
   } catch (error) {
-    console.log(error)
+    console.log(error);
     ErrorApp.handleServiceError(error, "Error al obtener notas de crédito");
   }
-}
+};
 
 module.exports = {
-    emitirNotaDeCredito,
-    getNotasDeCredito
-}
+  emitirNotaDeCredito,
+  getNotasDeCredito,
+};
